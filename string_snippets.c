9f8936829f5a42e01d59a783a3745cfb2c65be5e
Setuini <felix.schett@student.uibk.ac.at>
2017-01-29 16:57:50 +0100

    duplicate replicate reverse pointer version

--- string_snippets.c
+++ string_snippets.c
@@ -9,25 +9,56 @@ char* duplicate(char* str);
 char* replicate(char* str, int times);
 char* reverse(char* str);
 int str_to_int(char* str);
+void duplicate_ptr(char** strptr);
+void replicate_ptr(char** strptr, int times);
+void reverse_ptr(char** strptr);
 char* userinput();
 
 int main(int argc, char const **argv){
 	
-	char* str = duplicate("Hello");
-	printf("%s\n",str);
+	char test[] = "Hello";
+	char test2[] = "1234";
 
-	char* str2 = replicate("Hello",5);
-	printf("%s\n",str2);
+	//Functions with return
+	char* str = duplicate(test);
+	printf("Duplicate:           %s\n",str);
+	free(str);
 
-	char* str3 = reverse("World");
-	printf("%s\n",str3);
+	char* str2 = replicate(test,5);
+	printf("Replicate:           %s\n",str2);
+	free(str2);
 
-	int int1 = str_to_int("1244");
-	printf("%d\n",int1);
+	char* str3 = reverse(test);
+	printf("Reverse:             %s\n\n",str3);
+	free(str3);
 
+	//Functions with pointer passing
+	char* str4 = test;
+	char** strptr = &str4;
+
+	duplicate_ptr(strptr);
+	printf("Duplicate Pointer:   %s\n",*strptr);
+
+	*strptr = "Hello"; //reset the string to Hello
+	replicate_ptr(strptr, 5);
+	printf("Replicate Pointer:   %s\n",*strptr);
+
+	*strptr = "Hello"; //reset the string to Hello
+	reverse_ptr(strptr);
+	printf("Reverse Pointer:     %s\n\n",*strptr);
+
+	free(*strptr);
+
+	//String to int
+	int int1 = str_to_int(test2);
+	printf("String to int:       %d\n\n",int1);
+
+
+	//Userinput
 	char* userin = userinput();
-	printf("%s\n",userin);
-
+	printf("User Input:          %s\n\n",userin);
+	free(userin);
+	
 	return EXIT_SUCCESS;
 }
 
@@ -72,6 +103,49 @@ char* reverse(char* str){
 	return retval;
 }
 
+void duplicate_ptr(char** strptr){
+	int len = strlen(*strptr);
+	char* new = (char *)malloc( (sizeof(char) * len * 2) + 1); // +1 for \0
+	if (new == NULL){
+		fprintf(stderr, "ERROR duplicate - Allocation\n");
+		exit(EXIT_FAILURE);
+	}
+	strcpy(new, *strptr);
+	strcat(new, *strptr);
+	new[len*2] = '\0';
+	*strptr = new;
+}
+
+void replicate_ptr(char** strptr, int times){
+	int len = strlen(*strptr);
+	char* new = (char *)malloc( (sizeof(char) * len * times) + 1); // +1 for \0
+	if (new == NULL){
+		fprintf(stderr, "ERROR duplicate - Allocation\n");
+		exit(EXIT_FAILURE);
+	}
+	strcpy(new, *strptr);
+	for (int i = 0; i < times; i++){
+		strcat(new, *strptr);
+	}
+	new[len*times] = '\0';
+	*strptr = new;
+}
+
+void reverse_ptr(char** strptr){
+	int len = strlen(*strptr);
+	char* new = (char *)malloc( (sizeof(char) * len) + 1); // +1 for \0
+	if (new == NULL){
+		fprintf(stderr, "ERROR duplicate - Allocation\n");
+		exit(EXIT_FAILURE);
+	}
+	int j = len-1;
+	for (int i = 0; i < len; i++){
+		new[j--] = (*strptr)[i];
+	}
+	new[len] = '\0';
+	*strptr = new;
+}
+
 //Parses a int from a string - Example "100" -> 100
 int str_to_int(char* str){
 	char* end;
@@ -92,7 +166,7 @@ int str_to_int(char* str){
 
 //Get input from stdin
 char* userinput(){
-	printf("PLease input a string.\n");
+	printf("Please input a string.\n");
 	char* buffer = (char *)malloc(sizeof(char)*LINE_MAX);
 	if (buffer == NULL){
 		fprintf(stderr, "ERROR userinput - Allocation\n");
